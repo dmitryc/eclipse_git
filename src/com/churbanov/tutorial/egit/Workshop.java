44c9c4c29b69d69cb9bb0b8a873a4356b8c11ec9
Dmitry Churbanov <dmitry.churbanov@gmail.com>
2010-08-08 01:36:19 -0700

    Adding Workshop and Part classes.

---
+++ src/com/churbanov/tutorial/egit/Workshop.java
@@ -0,0 +1,15 @@
+package com.churbanov.tutorial.egit;
+
+/**
+ * Represents workshop where different works with cars, boats 
+ * and other vehicles can be done.
+ * 
+ * @author Dmitry Churbanov
+ */
+public class Workshop {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to our Workshop!");
+	}
+	
+}
